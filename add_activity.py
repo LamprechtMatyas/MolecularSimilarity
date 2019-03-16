885c08b5aec1ca7a5dc4d87b48a97575f892bcb5
LamprechtMatyas <45575382+LamprechtMatyas@users.noreply.github.com>
2019-03-16 14:33:30 +0100

    Changed method ordering

--- add_activity.py
+++ add_activity.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """"
 add real activity to similarity
 
@@ -12,29 +13,37 @@ Usage:
 import argparse
 import json
 
-import my_library
+import inputoutput_utils
+
+
+def _main():
+    configuration = _read_configuration()
+    activity = read_activity(configuration["activity"])
+    add_activity_and_write_to_json(configuration["score"], activity, configuration["output"])
 
 
 def _read_configuration() -> dict:
-    parser = argparse.ArgumentParser(description="add activity "
-                                                 "See file header for more details.")
-    parser.add_argument("-s", type=str, dest="score", required=True)
-    parser.add_argument("-a", type=str, dest="activity", required=True)
-    parser.add_argument("-o", type=str, dest="output", required=True)
+    parser = argparse.ArgumentParser(description="add activity ")
+    parser.add_argument("-s", type=str, dest="score",
+                        help="input json lines file from score_molecules.py", required=True)
+    parser.add_argument("-a", type=str, dest="activity",
+                        help="input json file with test activity",required=True)
+    parser.add_argument("-o", type=str, dest="output",
+                        help="output json lines file", required=True)
     return vars(parser.parse_args())
 
 
 def read_activity(input_activity: str) -> list:
-    with open(input_activity, "r") as stream:
+    with open(input_activity, "r", encoding="utf-8") as stream:
         for line in stream:
             activity = json.loads(line)
             return activity["activity"]
 
 
-def write_to_json(input_score: str, activity: list, output_file: str):
-    my_library.create_parent_directory(output_file)
-    with open(output_file, "w") as output_stream:
-        with open(input_score, "r") as stream:
+def add_activity_and_write_to_json(input_score: str, activity: list, output_file: str):
+    inputoutput_utils.create_parent_directory(output_file)
+    with open(output_file, "w", encoding="utf-8") as output_stream:
+        with open(input_score, "r", encoding="utf-8") as stream:
             for num, line in enumerate(stream):
                 score = json.loads(line)
                 output = {
@@ -45,12 +54,6 @@ def write_to_json(input_score: str, activity: list, output_file: str):
                 if num != 0:
                     output_stream.write("\n")
                 json.dump(output, output_stream)
-
-
-def _main():
-    configuration = _read_configuration()
-    activity = read_activity(configuration["activity"])
-    write_to_json(configuration["score"], activity, configuration["output"])
 
 
 if __name__ == "__main__":
