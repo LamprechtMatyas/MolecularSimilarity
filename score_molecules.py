2fda1bf173b7ccbc57561b58c7f7e1defd965a78
LamprechtMatyas <45575382+LamprechtMatyas@users.noreply.github.com>
2019-03-16 14:29:43 +0100

    Added main method

--- score_molecules.py
+++ score_molecules.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """"
 Molecules scoring
 
@@ -13,29 +14,31 @@ import argparse
 import json
 
 
+def _main():
+    configuration = _read_configuration()
+    with open(configuration["model"], "r", encoding="utf-8") as input_stream:
+        model_content = json.load(input_stream)
+
+    model = model_factory.create_model(model_content["configuration"]["model_name"])
+    model.score_model(model_content, configuration["fragments"],
+                      configuration["descriptors"], configuration["output"])
+
+
 def _read_configuration() -> dict:
     parser = argparse.ArgumentParser(description="Molecule scoring "
                                              "See file header for more details.")
     parser.add_argument("-m", type=str, dest="model",
-                        help="input model", required=True)
+                        help="input jso file with model", required=True)
     parser.add_argument("-f", type=str, dest="fragments",
-                        help="input test fragments", required=True)
+                        help="input json file with test fragments", required=True)
     parser.add_argument("-d", type=str, dest="descriptors",
-                        help="input test descriptors", required=True)
+                        help="input csv file with test descriptors", required=True)
     parser.add_argument("-o", type=str, dest="output",
                         help="output file", required=True)
     return vars(parser.parse_args())
 
 
-configuration = _read_configuration()
-with open(configuration["model"]) as input_stream:
-    model_content = json.load(input_stream)
-
-model = model_factory.create_model(model_content["metadata"]["name"])
-model.score_model(model_content["data"], configuration["fragments"], configuration["descriptors"],
-                  configuration["output"])
-
-
-
+if __name__ == "__main__":
+    _main()
 
 
