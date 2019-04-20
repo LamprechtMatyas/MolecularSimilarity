ac979b8fdf04b4788448240530646e644b8a9a22
LamprechtMatyas <45575382+LamprechtMatyas@users.noreply.github.com>
2019-04-20 16:48:35 +0200

    Changed ordering and input parameters

--- run_all_scripts.py
+++ run_all_scripts.py
@@ -38,10 +38,47 @@ import compute_evaluation
 def _main():
     # run extract_fragments
     configuration = _read_configuration()
+    
+    with open(configuration["model_configuration"], "r", encoding="utf-8") as input_stream:
+        model_configuration = json.load(input_stream)
+    try:
+        new_model = model_factory.create_model(model_configuration["model_name"])
+    except:
+        print("Wrong name of a model!!")
+        exit(1)
+        
+    if "kekule" not in model_configuration:
+        model_configuration["kekule"] = False
+    else:
+        model_configuration["kekule"] = bool(model_configuration["kekule"])
+    if "isomeric" not in model_configuration:
+        model_configuration["isomeric"] = False
+    else:
+        model_configuration["isomeric"] = bool(model_configuration["isomeric"])
+    if "fragments" not in model_configuration:
+        model_configuration["fragments"] = "ecfp.6"
+    parsed_types = []
+    for item in model_configuration["fragments"].split(","):
+        item_split = item.split(".")
+        if item_split[0] != "ap":
+            if not len(item_split) == 2:
+                logging.error("Invalid fragment type: %s", item)
+                logging.info("Expected format {TYPE}.{SIZE} or ap")
+                exit(1)
+            parsed_types.append({
+                "name": item_split[0],
+                "size": int(item_split[1])
+            })
+        else:
+            parsed_types.append({
+                "name": item_split[0],
+            })
+    model_configuration["fragments"] = parsed_types
+                
     extraction_options = {
-        "kekule": configuration["kekule"],
-        "isomeric": configuration["isomeric"],
-        "fragments": configuration["fragments"]
+        "kekule": model_configuration["kekule"],
+        "isomeric": model_configuration["isomeric"],
+        "fragments": model_configuration["fragments"]
     }
     input_files = [configuration["input_actives"], configuration["input_inactives"],
                    configuration["test"]]
@@ -54,21 +91,20 @@ def _main():
                                         fragments_output_files, extraction_options)
 
     # run extract_descriptors
-    with open(configuration["model_configuration"], "r", encoding="utf-8") as input_stream:
-        model_configuration = json.load(input_stream)
+    
     descriptors_output_files = [directory+"/descriptorsa.csv", directory+"/descriptorsi.csv",
                                 directory+"/descriptorst.csv"]
     for file in descriptors_output_files:
         inputoutput_utils.create_parent_directory(file)
-    if (model_configuration["model_name"] == "decision_tree_model") |\
-        ((model_configuration["model_name"] == "linear_regression_model") and (int(model_configuration["molecules"]) == 1)):
+    if (model_configuration["model_name"] == "descriptors_model") |\
+        ((model_configuration["model_name"] == "linear_regression_model") and (int(model_configuration["molecules"]) == 0)):
         compute_descriptors.compute_descriptors(fragments_output_files, descriptors_output_files,
-                                                False)
+                                                True)
     else:
-        compute_descriptors.compute_descriptors(fragments_output_files, descriptors_output_files, True)
+        compute_descriptors.compute_descriptors(fragments_output_files, descriptors_output_files, False)
 
     # run create_model and score_molecules
-    new_model = model_factory.create_model(model_configuration["model_name"])
+    
     model = new_model.create_model(directory+"/fragmentsa.json", directory+"/fragmentsi.json",
                                    directory+"/descriptorsa.csv", directory+"/descriptorsi.csv",
                                    model_configuration)
@@ -105,41 +141,16 @@ def _read_configuration() -> dict:
                         help="input json file with model configuration", required=True)
     parser.add_argument("-o", type=str, dest="output",
                         help="output json file", required=True)
-    parser.add_argument("-f", type=str, dest="fragments",
-                        help="optional, comma separated list of fragments", required=False)
     parser.add_argument("-p", type=str, dest="input_type",
                         help="type of input files with molecules smi/sdf, default is smi",
                         default="smi")
     parser.add_argument("-d", dest="directory",
                         help="directory where to store intermediate results", required=False)
-    parser.add_argument("--kekule", dest="kekule", help="kekule option",
-                        action="store_true", required=False)
-    parser.add_argument("--isomeric", dest="isomeric", help="isomeric option",
-                        action="store_true", required=False)
 
     configuration = vars(parser.parse_args())
-    if configuration["fragments"] is None:
-        configuration["fragments"] = "tt.3"
     if configuration["directory"] is None:
         configuration["directory"] = tempfile.gettempdir()
-    parsed_types = []
-    for item in configuration["fragments"].split(","):
-        item_split = item.split(".")
-        if item_split[0] != "ap":
-            if not len(item_split) == 2:
-                logging.error("Invalid fragment type: %s", item)
-                logging.info("Expected format {TYPE}.{SIZE} or ap")
-                exit(1)
-            parsed_types.append({
-                "name": item_split[0],
-                "size": int(item_split[1])
-            })
-        else:
-            parsed_types.append({
-                "name": item_split[0],
-            })
-
-    configuration["fragments"] = parsed_types
+    
     configuration["input_type"] = configuration["input_type"].lower()
     return configuration
 
