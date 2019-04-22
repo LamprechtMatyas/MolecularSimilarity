0e3691d4234411e4e897fe06bd6263a747652e46
LamprechtMatyas <45575382+LamprechtMatyas@users.noreply.github.com>
2019-04-22 21:37:17 +0200

    Added detection of wrong inputs

--- select_best_results.py
+++ select_best_results.py
@@ -22,7 +22,10 @@ def _main():
     elif configuration["type"] == "EF5":
         str1 = "AUC"
         str2 = "EF1"
-
+    else:
+        print("Wrong type!")
+        print("It has to be: AUC. EF1 or EF5")
+        exit(1)
     with open(configuration["input_file"], "r", encoding="utf-8") as input_stream:
         for new_line in input_stream:
             line = json.loads(new_line)
@@ -30,6 +33,12 @@ def _main():
             values[1].append(line[configuration["type"]])
             values[2].append(line[str1])
             values[3].append(line[str2])
+    if len(values[0]) < int(configuration["best"]):
+        print("The input file does not have that much good results!")
+        print("Number of results in input file: " + str(len(values[0])))
+        print("Number you wanted to select: " + configuration["best"])
+        print("Can not be like that!")
+        exit(1)
     for i in range(len(values[1]) - 1):
         for j in range(len(values[1]) - i - 1):
             if values[1][j] < values[1][j+1]:
@@ -63,7 +72,7 @@ def _read_configuration():
     parser.add_argument("-f", type=str, dest="input_file",
                         help="file from which you want to select best", required=True)
     parser.add_argument("-b", type=str, dest="best", help="number of best results", required=True)
-    parser.add_argument("-t", type=str, dest="type", help="type - AUC, EF", required=True)
+    parser.add_argument("-t", type=str, dest="type", help="type - AUC, EF1 or EF5", required=True)
     parser.add_argument("-o", type=str, dest="output", help="output_file", required=True)
 
     configuration = vars(parser.parse_args())
