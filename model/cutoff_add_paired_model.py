76ddce3a4f121e9f0fe690436a2e573aa93f6f7f
LamprechtMatyas <45575382+LamprechtMatyas@users.noreply.github.com>
2019-05-04 22:48:21 +0200

    Changed description

--- model/cutoff_add_paired_model.py
+++ model/cutoff_add_paired_model.py
@@ -6,7 +6,7 @@ pair is also increased. Then is done cutoff, which means that model only takes i
 more percentage threshold than is inputted cutoff. Then this pair thing is also applied on test
 molecules and then the similarity is computed.
 input model_configuration should look like this:
-    {"model_name": "cutoff_add_paired_model", "cutoff":num, "pair": [num1,  num2]}
+    {"model_name": "cutoff_add_paired_model", "fragments": "ecfp.6", "cutoff": num, "pair": [num1,  num2]}
     num is between 0 and 100 and it simulates percentage
 """
 
@@ -67,7 +67,7 @@ class CutoffAddPairedModel(IModel):
                 "pair": model_configuration["pair"]
             },
             "data": {
-                "active": [cutoff_indexes]
+                "active": cutoff_indexes
             }
         }
         return model
@@ -77,8 +77,48 @@ class CutoffAddPairedModel(IModel):
 
     def score_model(self, model_configuration: list, fragments_file: str,
                     descriptors_file: str, output_file: str):
-        model = paired_model.AddPairedModel()
-        model.score_model(model_configuration, fragments_file, descriptors_file, output_file)
+        inputoutput_utils.create_parent_directory(output_file)
+        first_line = True
+        with open(output_file, "w", encoding="utf-8") as output_stream:
+            with open(fragments_file, "r", encoding="utf-8") as input_stream:
+                for new_line in input_stream:
+                    line = json.loads(new_line)
+                    test_active_indexes = _add_pair(line["fragments"],
+                                                    int(model_configuration["configuration"][
+                                                            "pair"][0]),
+                                                    int(model_configuration["configuration"][
+                                                            "pair"][1]))
+                    max_sim = _compute_sim(model_configuration["data"]["active"], test_active_indexes)
+                    score = {
+                        "name": line["name"],
+                        "score": max_sim
+                    }
+                    if first_line:
+                        first_line = False
+                    else:
+                        output_stream.write("\n")
+                    json.dump(score, output_stream)
+
+
+def _add_pair(fragments: dict, pair1: int, pair2: int) -> list:
+    molecule_indexes = []
+    for item in fragments:
+        if item["index"] not in molecule_indexes:
+            molecule_indexes.append(item["index"])
+    if (pair1 not in molecule_indexes) and (pair2 in molecule_indexes):
+        molecule_indexes.append(pair1)
+    elif (pair1 in molecule_indexes) and (pair2 not in molecule_indexes):
+         molecule_indexes.append(pair2)
+    return molecule_indexes
+
+
+def _compute_sim(active_fragments: list, test_fragments: list) -> list:
+    summary = 0
+    for item in test_fragments:
+        if item in active_fragments:
+            summary += 1
+    sim = summary / (len(active_fragments) + len(test_fragments) - summary)
+    return sim
 
 
 register_model(CutoffAddPairedModel.model_name, lambda: CutoffAddPairedModel())
