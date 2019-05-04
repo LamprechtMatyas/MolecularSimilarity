90e59d08660187f55505c9d7fae3af905560afd9
LamprechtMatyas <45575382+LamprechtMatyas@users.noreply.github.com>
2019-05-04 20:46:24 +0200

    Improved description

--- run_all_scripts.py
+++ run_all_scripts.py
@@ -10,8 +10,13 @@ Usage:
         -c (optional input file with activity of test molecules)
         -m (input configuration json file)
         -o (output file)
-        -p (optional, type of input molecules files, default smi)
+        -p (optional, type of input molecules files, default sdf)
         -d (optional directory where to store intermediate results)
+Fragments type:
+    - tt.{SIZE}
+    - ecfp.{SIZE}
+    - fcfp.{SIZE}
+    - ap
 
 """
 import argparse
